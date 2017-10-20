936bbd559f3289b82395a03d320d4fdd616fcf90
Ari-M <ariminton@live.com>
2017-10-20 10:36:20 -0700

    first commit

--- src/Main.js
+++ src/Main.js
@@ -1,41 +1,48 @@
 import React, { Component } from 'react';
 import Selection from './Selection';
 import ComparisonList from './ComparisonList';
+import $ from 'jquery'
 
 class Main extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      items: ['selection', 'list'], // items received from the API fetch
+      selectedItem: {
+          name: 'Batman Costume', // some sort of name we can display as a title
+          imgUrl: 'https://target.scene7.com/is/image/Target/51208174?wid=520&hei=520&fmt=pjpeg', // link to the image so we can display it
+          webUrl: 'https://www.target.com/p/batman-the-dark-knight-rises-men-s-muscle-chest-deluxe-adult-costume-x-large/-/A-51208174?ref=tgt_adv_XS000000&AFID=google_pla_df&CPNG=PLA_Seasonal+Shopping&adgroup=SC_Seasonal&LID=700000001170770pgs&network=g&device=c&location=9033309&gclid=Cj0KCQjwvabPBRD5ARIsAIwFXBlZEGZPtGy26Ak5Ssp41sDXAfn1HEt8W5W0aNf0xUUR-lA_eXK4nccaAtHHEALw_wcB&gclsrc=aw.ds', // link to the place where we can see the posting
+          cost: '$29.99' // Some sort of dollar information to display
+      }, // the current item being displayed, shifted from the front of the items array
     }
   }
 
   componentDidMount = () => {
-    (() => {
-      var cx = '000308025382460033617:c9agyvqp_o4';
-      var gcse = document.createElement('script');
-      gcse.type = 'text/javascript';
-      gcse.async = true;
-      gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(gcse, s);
-    })
+
   }
 
-  handleSearch = (e) => {
-    e.preventDefault();
+  handleSearch = () => {
+    // API fetch goes here
+    // Once JSON is received, we need the following pieces of data (if possible):
+    // item: {
+    //   name: 'Batman Costume', // some sort of name we can display as a title
+    //   imgUrl: 'https://google.com/img.png', // link to the image so we can display it
+    //   webUrl: 'https://google.com', // link to the place where we can see the posting
+    //   cost: '$29.99' // Some sort of dollar information to display
+    // }
+    // Afterwards, we need to shuffle the item array. Unshift the first item into a variable.
+    // Save the unshifted item in selectedItem. Save the rest of the item array in items
   }
 
   render() {
     return(
       <div>
-        <div className="gcse-searchbox" data-resultsUrl="http://www.google.com" data-newWindow="true" data-queryParameterName="search" >
-        </div>
-        <Selection handleSearch={'farge'}/>
-        <ComparisonList />
+        <Selection selectedItem={this.state.selectedItem} handleRejectio={this.props.handleRejection} handleApproval={this.props.handleApproval}/>
+        <ComparisonList comparisonList={this.props.comparisonList}/>
       </div>
     )
   }
 }
 
 export default Main;
+
